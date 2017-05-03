5e659573027ead9144d0c2b6bdb5261d8d8f417f
Afsoon.Nicknam <a.nicknam@gmail.com>
2017-05-03 13:29:11 -0700

    handle post message with img_url: push a message with img_url as text

--- index.js
+++ index.js
@@ -2,28 +2,37 @@ require('dotenv').config();
 const express = require('express');
 const port = process.env.PORT || 8100;
 const request = require('request');
-
-
-const app = express();
-
+const bodyParser = require('body-parser');
 const incomingWebhookURL = process.env.SLACK_WEBHOOK_URL;
+const app = express();
+app.use(bodyParser.json());
 
-let messageTemplate = {
+function _post(url, data) {
+	let options = {
+		url: url,
+		method: 'POST',
+		json: data
+	};
+	request(options, (err, res, body) => {
+		if (err) {
+			console.log(err);
+			// console.log('Failed post request. Status code = ', res.statusCode);
+		}
+	});
+}
+
+var messageTemplate = {
 	"username": "Afsoon",
-	"channel": "bot",
+	"channel": "bot2", //TODO Update Channel
 	"icon_emoji": ":rage:",
 	"text": "Really?????"
 }
 
 app.post('/push/img', (req, res) => {
 	let img_url = req.body.img_url;
-	request(incomingWebhookURL, function(err, res, body) {  
-		if (err) {
-			console.error(err);
-		}
-	    
-		}
-	});
+	let message = Object.assign(messageTemplate);
+	message["text"] = img_url;
+	_post(incomingWebhookURL, messageTemplate);
 
 });
 
