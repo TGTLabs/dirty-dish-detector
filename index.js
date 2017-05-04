84279548ba42d4c02b13c38be6a7cf9b0408e85e
Afsoon.Nicknam <a.nicknam@gmail.com>
2017-05-04 12:38:52 -0700

    code clean up

--- index.js
+++ index.js
@@ -18,6 +18,23 @@ app.get('*', function(req, res){
 	res.sendFile(path.join(__dirname, './www/index.html'));
 });
 
+function _post(url, data) {
+	return new Promise((resolve, reject) => {
+		let options = {
+			url: url,
+			method: 'POST',
+			json: data
+		};
+		request(options, (err, res, body) => {
+			if (err) {
+				reject(err);
+			} else {
+				resolve(res);
+			}
+		});
+	}); 
+}
+
 var messageTemplate = {
 	"username": "mugshots",
 	"channel": "bot", //TODO Update Channel
@@ -25,7 +42,6 @@ var messageTemplate = {
 }
 
 app.post('/upload', upload.single('mugshot'), (req, res) => {
-
 	let message = {};
 	for (let key in messageTemplate) {
 		message[key] = messageTemplate[key];
@@ -82,63 +98,6 @@ app.post('/upload', upload.single('mugshot'), (req, res) => {
 	});
 
 });
-
-
-function _post(url, data) {
-	return new Promise((resolve, reject) => {
-		let options = {
-			url: url,
-			method: 'POST',
-			json: data
-		};
-		request(options, (err, res, body) => {
-			if (err) {
-				reject(err);
-			} else {
-				resolve(res);
-			}
-		});
-	}); 
-}
-
-
-
-// app.post('/push/img', (req, res) => {
-// 	let message = {};
-// 	for (let key in messageTemplate) {
-// 		message[key] = messageTemplate[key];
-// 	}
-// 	let msgAttachments = [];
-// 	msgAttachments.push({
-// 		"title": "Shame on you!",
-// 		"text": "for leaving dirty dishes in the kitchen sink! \nHere's a headshot of the worst employee of the month!",
-// 		// "image_url": img_url
-// 		"callback_id": "wopr_game",
-//     "color": "#3AA3E3",
-//     "attachment_type": "default",
-//     "actions": [{
-// 	    "name": "game",
-// 	    "text": "Approve",
-// 	    "type": "button",
-// 	    "value": "yes"
-//     },{
-//     	"name": "game",
-//     	"text": "Deny",
-//     	"type": "button",
-//     	"value": "no"
-//     }]
-// 	}); 
-// 	message["attachments"] = msgAttachments;
-// 	_post(incomingWebhookURL, message)
-// 	.then(() => {
-// 		res.sendStatus(200);
-// 	}).catch((err) => {
-// 		console.log('Failed post request. Status code = ', res.statusCode);
-// 	});
-// });
-
-
-
 
 app.listen(port, '0.0.0.0' ,() => {
 	console.log(`server listening on ${port}`);
